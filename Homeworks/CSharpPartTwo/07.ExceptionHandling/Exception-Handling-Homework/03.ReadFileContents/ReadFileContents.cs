3df1e8ad6cbe4942b380d7583af5ab2861be9040
SHAMMY1 <shammy.bg1@gmail.com>
2015-02-24 17:25:04 +0200

    Created c sharp part two Exception-Handling-Homework solution and projects for all tasks

---
+++ Homeworks/CSharpPartTwo/07.ExceptionHandling/Exception-Handling-Homework/03.ReadFileContents/ReadFileContents.cs
@@ -0,0 +1,22 @@
+﻿//Problem 3. Read file contents
+
+//Write a program that enters file name along with its full file path (e.g. C:\WINDOWS\win.ini),
+//reads its contents and prints it on the console.
+//Find in MSDN how to use System.IO.File.ReadAllText(…).
+//Be sure to catch all possible exceptions and print user-friendly error messages.
+
+using System;
+
+class ReadFileContents
+{
+	static void Main()
+	{
+		string task = "Problem 3. Read file contents\n\nWrite a program that enters file name along with its full file path\n(e.g. C:\\WINDOWS\\win.ini), reads its contents and prints it on the console.\nFind in MSDN how to use System.IO.File.ReadAllText(…).\nBe sure to catch all possible exceptions and print user-friendly error messages.\n";
+
+		string separator = new string('*', Console.WindowWidth);
+
+		Console.WriteLine(task);
+		Console.WriteLine(separator);
+
+	}
+}
---
+++ Homeworks/CSharpPartTwo/07.ExceptionHandling/Exception-Handling-Homework/04.DownloadFile/DownloadFile.cs
@@ -0,0 +1,22 @@
+﻿//Problem 4. Download file
+
+//Write a program that downloads a file from Internet (e.g. Ninja image,http://telerikacademy.com/Content/Images/news-img01.png)
+//and stores it the current directory.
+//Find in Google how to download files in C#.
+//Be sure to catch all exceptions and to free any used resources in the finally block.
+
+using System;
+
+class DownloadFile
+{
+	static void Main()
+	{
+		string task = "Problem 4. Download file\n\nWrite a program that downloads a file from Internet (e.g. Ninja image)\nand stores it the current directory.\nFind in Google how to download files in C#.\nBe sure to catch all exceptions and to free any used resources in the finally\nblock.\n";
+
+		string separator = new string('*', Console.WindowWidth);
+
+		Console.WriteLine(task);
+		Console.WriteLine(separator);
+
+	}
+}
